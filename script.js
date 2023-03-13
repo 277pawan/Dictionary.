8cedf11cac5e62f71a4d218b93e10acae20cefa0
Pawan Bisht <124587748+277pawan@users.noreply.github.com>
2023-03-13 16:57:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,24 @@
 'use strict'
 
-
-
-
 const url="https://api.dictionaryapi.dev/api/v2/entries/en/";
-
 const url1= "https://google-translate1.p.rapidapi.com/language/translate/v2/languages";
-
 const btn=document.getElementById("search-btn");
-
 const contain=document.querySelector(".container");
-
 const result=document.getElementById("result");
-
 const wordday=document.querySelector(".modal-content");
-
 const sound=document.getElementById("sound");
-
 const audio=document.getElementsByTagName('audio'); 
-
-
-
-
-// Way of generating a fetch call with a XML.
-
-
-
 // const getsearchdata=function (search){
 //     *******Fetching the code using old method **********
 // function render2(){
 //   const wwe=new XMLHttpRequest();
 //   wwe.open("GET",`https://api.dictionaryapi.dev/api/v2/entries/en/hello`,
 //   true);
-//   wwe.onreadystatechange = function()
-// {
-//       if (wwe.readyState === XMLHttpRequest.DONE) 
-//   {
-//         if (wwe.status === 200) 
-//     {
+//   wwe.onreadystatechange = function() {
+//       if (wwe.readyState === XMLHttpRequest.DONE) {
+//         if (wwe.status === 200) {
 //           console.log(wwe.responseText);
-//         }else
-//       {
+//         } else {
 //           // console.log('Error: ' + request.status + ' ' + request.statusText);
 //         }
 //       }
@@ -76,9 +54,7 @@ const audio=document.getElementsByTagName('audio');
 
 
 
-
-function render()
-{
+function render(){
 let inpWord=document.getElementById("inp-word").value;
  fetch(`${url}${inpWord}`)
 .then((response)=>response.json())
@@ -105,8 +81,7 @@ play();
 });
 };
 // sound.forEach(icon1 => {
-// icon1.addEventListener("click",({target})=>
-{
+// icon1.addEventListener("click",({target})=>{
 //   if(target.id=='sound')
 //   {
 //     let utter;
@@ -117,8 +92,6 @@ play();
 //   });
 // }
 // );
-
-
 
 btn.addEventListener('click',()=>{
   render()
@@ -157,26 +130,12 @@ document.addEventListener('keydown',function(e){
 })
 
 
-
-
-
-
 // Random word API.
-
-
-
-
-
-
-
-
 const uni=new XMLHttpRequest();
 uni.open("GET",`https://random-word-api.herokuapp.com/word`,
   true);
-  uni.onreadystatechange = function()
-  {
-      if (uni.readyState === XMLHttpRequest.DONE)
-      {
+  uni.onreadystatechange = function() {
+      if (uni.readyState === XMLHttpRequest.DONE) {
         if (uni.status === 200) {
           console.log(uni.responseText);
         } else {
@@ -186,33 +145,18 @@ uni.open("GET",`https://random-word-api.herokuapp.com/word`,
     };
   
   uni.send();
-
-   uni.addEventListener('load',function()
-{
+   uni.addEventListener('load',function(){
 const data=JSON.parse(this.responseText);
 console.log(data);
-     
 wordday.innerHTML=` <center style="color:blue;font-size:50px;"><u>Word of the day</u></center> <br>
 <p style="text-align:center;color:#212c6c;font-size:30px;">${data}</p>`
-
-   });
+});
 // Random word API ends Headers.
 
 
 
-
-
-
-
-
-
-
-
-
 const modal = document.getElementById("myModal");
-
 const modalclose=document.querySelector(".close");
-
 const day=document.getElementById("wordofday");
 
 
@@ -221,22 +165,19 @@ window.addEventListener("load", function() {
 
 });
 
-modalclose.onclick = function() 
-{
+modalclose.onclick = function() {
   modal.style.display = "none";
 }
 
 
 
-window.onclick = function(event)
-{
+window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-document.addEventListener('keydown',function(e)
-                          {
+document.addEventListener('keydown',function(e){
   if(e.key==="Escape")
   {
     modal.style.display = "none";
@@ -246,73 +187,19 @@ day.addEventListener('click',function()
 {
     modal.style.display = "block";
 
-  window.onclick = function(event) 
-  {
+  window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
   }
   
-  document.addEventListener('keydown',function(e)
-                            {
+  document.addEventListener('keydown',function(e){
     if(e.key==="Escape")
     {
       modal.style.display = "none";
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // API for translating the english to hindi .
